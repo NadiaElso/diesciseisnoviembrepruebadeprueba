ea94bb50a6e23e6c6db4f242a9ef5bc7184dec65
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2021-11-16 14:24:12 -0300

    otrointento

--- src/index.ts
+++ src/index.ts
@@ -15,36 +15,51 @@ for (let index = 0; index < año.length; index++) {
 
 
 
-let mes:any=new Array(12);
+let mes30dias:any[]=new Array();
+let mes31dias:any[]=new Array();
+let mes28dias:any[]=new Array();
+mes30dias[]=[];
+mes31dias[]=[];
+mes28dias[]=[];
 
-let indicemes:number[]=new Array(1);
+let indicemes:number[]=new Array();
 
 for (let index = 0; index < indicemes.length; index++) {
   indicemes[index]=Number(prompt("Ingresa mes"));
   for (let index = 0; index < mes.length; index++) {
 
-   mes = [1, 2, 3, 4, 5,6,7,8,9,10,11,12];
+ 
+
+    if (mes[index]===4||mes[index]===6||mes[index]===9||mes[index]===11) {
+      mes30dias[index]=30;
+    
+      mes = [1, 2, 3, 4, 5,6,7,8,9,10,11,12];
  indicemes = mes.reduce((a, b) => a + b, 0);
 
 console.log(indicemes);
-
-    if (mes[index]===4||mes[index]===6||mes[index]===9||mes[index]===11) {
-      mes=30;
-    
-    
     
     } else if(mes[index]===1||mes[index]===3||mes[index]===5||mes[index]===7||mes[index]===8||mes[index]===10||mes[index]===12) {
-      mes=31;
-      
+      mes31dias[index]=31;
+      mes = [1, 2, 3, 4, 5,6,7,8,9,10,11,12];
+      indicemes = mes.reduce((a, b) => a + b, 0);
+     
+     console.log(indicemes);
       
     
     }else if(mes[index]===2&&año[index]===366){
-          mes=29;
+          mes28dias[index]=29;
         
-     
+          mes = [1, 2, 3, 4, 5,6,7,8,9,10,11,12];
+ indicemes = mes.reduce((a, b) => a + b, 0);
+
+console.log(indicemes);
     
     }else  { 
-    mes=28;
+    mes28dias[index]=28;
+    mes = [1, 2, 3, 4, 5,6,7,8,9,10,11,12];
+    indicemes = mes.reduce((a, b) => a + b, 0);
+   
+   console.log(indicemes);
     
     }
     
